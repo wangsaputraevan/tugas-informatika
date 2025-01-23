b585dfd30e40c64c60c02a6c3450f997cda9e3ba
wangsaputraevan <wangsaputraevan@gmail.com>
2025-01-23 13:50:07 +0700

    Add files via upload

---
+++ nav-logo/script.js
@@ -0,0 +1,16 @@
+const dotCount = 40;
+const animationContainer = document.querySelector(".wrapper");
+console.log("js started");
+
+for (let i = 0; i < dotCount; i++) {
+    let divElement = document.createElement("div");
+    divElement.classList.add("dot");
+    const randomTop = Math.random() * 100;
+    const randomLeft = Math.random() * 100;
+    divElement.style.top = `${randomTop}%`;
+    divElement.style.left = `${randomLeft}%`;
+    const randomDuration = 5 + Math.random() * 15;
+    divElement.style.animation = `animation-css ${randomDuration}s linear infinite`;
+    divElement.style.borderRadius = "100%";
+    animationContainer.appendChild(divElement);
+}
